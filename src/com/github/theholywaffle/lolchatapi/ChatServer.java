3020d2563b5e5869c5a683c6ee48971c655960ab
Bert De Geyter <degeyter.bert@gmail.com>
2014-06-26 18:00:32 +0200

    Updated servers

--- src/com/github/theholywaffle/lolchatapi/ChatServer.java
+++ src/com/github/theholywaffle/lolchatapi/ChatServer.java
@@ -15,15 +15,17 @@ package com.github.theholywaffle.lolchatapi;
  *
  */
 public enum ChatServer {
-	
-	EUW("chat.eu.lol.riotgames.com"),
+
+	EUW("chat.euw1.lol.riotgames.com"),
 	NA("chat.na1.lol.riotgames.com"),
-	EUNE("chat.eun1.lol.riotgames.com"),
+	EUNE("chat.eun1.riotgames.com"),
+	PBE("chat.pbe1.lol.riotgames.com"),
+	OCE("chat.oc1.lol.riotgames.com"),
 	TW("chattw.lol.garenanow.com"),
 	TH("chatth.lol.garenanow.com"),
 	PH("chatph.lol.garenanow.com"),
 	VN("chatvn.lol.garenanow.com");
-	
+
 	String host;
 
 	ChatServer(String host) {
